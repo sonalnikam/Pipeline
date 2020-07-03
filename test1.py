fd0ff7490098b5d0cff06a8a65d7efbea28affb5
sonalnikam <sonalnikam08@gmail.com>
2020-07-03 19:38:08 +0530

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,22 +1,38 @@
-import unittest
-from scipy.special import factorial
-from factorial import fact
+import unittest 
 
-class TestFactorial(unittest.TestCase):
-    """
-    Our basic test class
-    """
+class TestStringMethods(unittest.TestCase): 
+	
+	def setUp(self): 
+		pass
 
-    def test_fact(self):
-        """
-        The actual test.
-        Any method which starts with ``test_`` will considered as a test case.
-        """
-        res = fact(5)
-        self.assertEqual(res, 120)
+	# Returns True if the string contains 4 a. 
+	def test_strings_a(self): 
+		self.assertEqual( 'a'*4, 'aaaa') 
 
+	# Returns True if the string is in upper case. 
+	def test_upper(self):		 
+		self.assertEqual('foo'.upper(), 'FOO') 
 
-if __name__ == '__main__':
-    unittest.main() 
+	# Returns TRUE if the string is in uppercase 
+	# else returns False. 
+	def test_isupper(self):		 
+		self.assertTrue('FOO'.isupper()) 
+		self.assertFalse('Foo'.isupper()) 
 
+	# Returns true if the string is stripped and 
+	# matches the given output. 
+	def test_strip(self):		 
+		s = 'geeksforgeeks'
+		self.assertEqual(s.strip('geek'), 'sforgeeks') 
 
+	# Returns true if the string splits and matches 
+	# the given output. 
+	def test_split(self):		 
+		s = 'hello world'
+		self.assertEqual(s.split(), ['hello', 'world']) 
+		with self.assertRaises(TypeError): 
+			s.split(2) 
+
+if __name__ == '__main__': 
+	unittest.main() 
+ 
